06c686e8869a134381028867ee6e6fe776b5b08c
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-03 11:04:34 -0700

    its-working

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log('Hello World!')
+
+const numButtons = document.querySelectorAll('.button')
+console.log('array of buttons', numButtons)
+
+const display = document.getElementById('display')
+console.log(display, display)
+
+for (let button of numButtons) {
+  button.addEventListener('click', (Event) => {
+    if (Event.target.id !== 'eval') {
+      display.innerText += Event.target.id
+      console.log('click', Event.target.id)
+    }
+    if (Event.target.id === 'clear') {
+      display.innerText = ''
+      console.log('Clear!')
+    }
+    if (Event.target.id === 'eval') {
+      let result = eval(display.innerText)
+      display.innerText = result
+      console.log(result)
+      console.log('Eval Clicked!')
+    }
+  })
+}
