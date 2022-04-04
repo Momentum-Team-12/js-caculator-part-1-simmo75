63e025569fd3407c9d76d5a43e8d586c3b73901f
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-04 08:58:24 -0700

    added-return-to-zero

--- main.js
+++ main.js
@@ -8,12 +8,17 @@ console.log(display, display)
 
 for (let button of numButtons) {
   button.addEventListener('click', (Event) => {
-    if (Event.target.id !== 'eval') {
-      display.innerText += Event.target.id
-      console.log('click', Event.target.id)
+    if (Event.target.id !== 'eval' &&  display.innerText === '0') {
+        display.innerText = Event.target.id
     }
+        else if (Event.target.id !== 'eval'){
+            display.innerText += Event.target.id
+            console.log('click', Event.target.id)    
+    }
+    
+
     if (Event.target.id === 'clear') {
-      display.innerText = ''
+      display.innerText = '0'
       console.log('Clear!')
     }
     if (Event.target.id === 'eval') {
